c07c1d469b916fd877762b9da5a4cc01738d0588
pYGOD3215 <108536518+pYGOD3215@users.noreply.github.com>
2023-06-17 13:13:50 +0100

    first commit

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>JS Mastery Store</title>
+        <title>pY-MALL</title>
       </Head>
       <header>
         <Navbar />
