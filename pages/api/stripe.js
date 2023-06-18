d725ebd8bce268d369bcf7c4788c96d7e153d01c
pYGOD3215 <108536518+pYGOD3215@users.noreply.github.com>
2023-06-18 13:57:50 +0100

    Final Push

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -11,11 +11,12 @@ export default async function handler(req, res) {
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
         shipping_options: [
-          { shipping_rate: 'shr_1Kn3IaEnylLNWUqj5rqhg9oV' },
+          { shipping_rate: 'shr_1NK4PqBRrAquKWrv8G8ZUvzW' },
+          { shipping_rate: 'shr_1NK4RXBRrAquKWrv9IGGFcNy' },
         ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
-          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/vfxfwnaw/production/').replace('-webp', '.webp');
+          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/ekot6n3h/production/').replace('-webp', '.webp');
 
           return {
             price_data: { 
